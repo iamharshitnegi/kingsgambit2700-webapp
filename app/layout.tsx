b95a9dd210c136fab0289f7a57cc29431424e872
Harshit Negi <harshit.negi@torusdigital.com>
2026-01-06 11:36:44 +0530

    added analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Merriweather } from "next/font/google";
 import "./globals.css";
 import FloatingWhatsApp from './components/FloatingWhatsapp';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const merriweather = Merriweather({ weight: ["300", "400", "700"], subsets: ["latin"], variable: "--font-merriweather" });
@@ -24,6 +26,8 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} ${merriweather.variable} font-sans text-slate-800 bg-brand-light antialiased`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
         <FloatingWhatsApp />
       </body>
     </html>
