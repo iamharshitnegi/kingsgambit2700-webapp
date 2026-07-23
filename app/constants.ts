de251cdcf12b122d46cd1dfd51555066240f282a
Harshit Negi <harshit.negi@torusdigital.com>
2026-07-23 11:22:59 +0530

    Updated email id and whatsapp number

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const WHATSAPP_NUMBER = "+917456985878";
-export const EMAIL = "harshit.igen@gmail.com";
+export const WHATSAPP_NUMBER = "+918679762608";
+export const EMAIL = "myemailid2700@gmail.com";
 export const INSTA_URL = "https://www.instagram.com/kingsgambit2700/";
